eab728803ef7f64bb468a7e9745aa36818b45c5a
khalid-11 <106479097+khalid-11@users.noreply.github.com>
2023-03-05 18:56:21 +0300

    code formatted

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const toggleNav = document.getElementById("toggle-nav");
-const toggleColse = document.getElementById("toggle-close")
+const toggleColse = document.getElementById("toggle-close");
 const overlay = document.querySelector(".overlay");
-console.log(toggleBtn);
 
 toggleBtn.addEventListener("click", () => {
   toggleNav.classList.add("show");
   toggleColse.classList.add("show");
-  overlay.style.display='block';
-
-})
+  overlay.style.display = "block";
+});
 toggleColse.addEventListener("click", () => {
   toggleNav.classList.remove("show");
-  toggleColse.classList.remove("show")
-  overlay.style.display='none';
-})
+  toggleColse.classList.remove("show");
+  overlay.style.display = "none";
+});
